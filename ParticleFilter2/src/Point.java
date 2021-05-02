de130d23d5afbaacf73d8f8cf405043e8fd24114
minehunter <alivalabarbaros@gmail.com>
2021-05-02 15:06:39 +0300

    Add files via upload

---
+++ ParticleFilter2/src/Point.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author Michael
+ */
+public class Point {
+    
+    public float x, y;
+
+    public Point() { }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+     
+}
